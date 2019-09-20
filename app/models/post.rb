d942179dfb10c27a6a1151e71398d028dae228fd
Tatsuki Shiga <45358873+tatsukishiga@users.noreply.github.com>
2019-09-20 16:10:22 +0900

    Revert "Creating a comment model"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
-  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :photos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_20_070141) do
-
-  create_table "comments", force: :cascade do |t|
-    t.text "comment", null: false
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_likes_on_post_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2019_09_17_141347) do
 
   create_table "photos", force: :cascade do |t|
     t.string "image", null: false
